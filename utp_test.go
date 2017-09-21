6618dd980b5454bb8568535d8d82a01aed310633
Matt Joiner <anacrolix@gmail.com>
2017-09-21 13:45:10 +1000

    Do nettest.TestConn with dialed and accepted connections in each position

--- utp_test.go
+++ utp_test.go
@@ -19,14 +19,16 @@ func init() {
 	log.SetFlags(log.Flags() | log.Lshortfile)
 }
 
-func TestNettestLocalhostUDP(t *testing.T) {
-	t.Parallel()
+func doNettestTestConn(t *testing.T, swapConns bool) {
 	nettest.TestConn(t, func() (c1, c2 net.Conn, stop func(), err error) {
 		s, err := NewSocket("udp", "localhost:0")
 		if err != nil {
 			return
 		}
 		c1, c2 = connPairSocket(s)
+		if swapConns {
+			c1, c2 = c2, c1
+		}
 		stop = func() {
 			s.Close()
 		}
@@ -34,12 +36,21 @@ func TestNettestLocalhostUDP(t *testing.T) {
 	})
 }
 
+func TestNettestTestConn(t *testing.T) {
+	t.Parallel()
+	doNettestTestConn(t, false)
+}
+
+func TestNettestTestConnSwapped(t *testing.T) {
+	t.Parallel()
+	doNettestTestConn(t, true)
+}
+
 var rander = rand.New(rand.NewSource(time.Now().UnixNano()))
 
-func connPairSocket(s *Socket) (net.Conn, net.Conn) {
+func connPairSocket(s *Socket) (dialed net.Conn, accepted net.Conn) {
 	var (
-		wg               sync.WaitGroup
-		dialed, accepted net.Conn
+		wg sync.WaitGroup
 	)
 	wg.Add(2)
 	go func() {
@@ -59,16 +70,7 @@ func connPairSocket(s *Socket) (net.Conn, net.Conn) {
 		}
 	}()
 	wg.Wait()
-	switch rander.Intn(2) {
-	case 0:
-		log.Printf("first of conn pair is dialed")
-		return dialed, accepted
-	case 1:
-		log.Print("first of conn pair is accepted")
-		return accepted, dialed
-	default:
-		panic("ಠ_ಠ")
-	}
+	return
 }
 
 const neverResponds = "localhost:1"
