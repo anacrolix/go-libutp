39b45e4bb50d8635d889e0b25792cccce02e0931
FIGBERT <figbert@figbert.com>
2022-01-29 22:50:57 -0800

    Simplify implementation of default logger

--- logging.go
+++ logging.go
@@ -1,30 +1,10 @@
 package utp
 
-import (
-	"fmt"
-	"os"
-	"time"
-
-	"github.com/anacrolix/log"
-)
+import "github.com/anacrolix/log"
 
 const (
 	logCallbacks = false
 	utpLogging   = false
 )
 
-var Logger = log.Logger{log.StreamLogger{
-	W: os.Stderr,
-	Fmt: func(msg log.Msg) []byte {
-		ret := []byte(fmt.Sprintf(
-			"%s go-libutp: %s",
-			time.Now().Format("2006-01-02T15:04:05-0700"),
-			msg.Text(),
-		))
-
-		if ret[len(ret)-1] != '\n' {
-			ret = append(ret, '\n')
-		}
-		return ret
-	},
-}}
+var Logger = log.Default.WithContextText("go-libutp")
